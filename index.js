76d852763d3d4b5f28946d882c87d1c863aa0de6
Sumailah Aziz <sumailahaziz@gmail.com>
2022-03-16 18:30:37 -0400

    UPDATED FILE

--- index.js
+++ index.js
@@ -31,13 +31,17 @@ const promptUser = () => {
             type:"list",
             message: "What is the license for this project?",
             name: "License",
-            choices: [
-                "MIT [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)", 
-                "APACHE 2.0 [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)", 
-                "artistic-2.0 [![License: Artistic-2.0](https://img.shields.io/badge/License-Artistic%202.0-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)", 
-                "bsl-1.0 [![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)" , 
-                "None"],
-        },{
+            choices: ['Apache 2.0', 'MIT License', 'Mozilla Public License 2.0', 'Boost Software License 1.0','None'],
+            validate: (licenseInput) => {
+                if (licenseInput) {
+                    return true;
+                } else {
+                    console.log ("Choose a license for your project.");
+                    return false;
+                }
+        }
+        },
+        {
             type:"input",
             name: "Contributing",
             message: "How can users contribute to this project?",
@@ -47,7 +51,7 @@ const promptUser = () => {
             message: "Give test instructions",
         },{
             type:"input",
-            name: "Questions",
+            name: "Contact",
             message: "Enter your github username.",
         },
         {
@@ -69,6 +73,16 @@ const promptUser = () => {
 
 const writeToFile = answers => {
     writeFileAsync('README.md', generateMarkdown(answers))
+}
+
+function init() {
+    // Use the questions to prompt the user in command line
+    inquirer.prompt(questions)
+        // Use the user's answers to generate the markdown for the README.md and output it to a new directory
+        .then((answers) => {
+            const readmeContent = generateMarkdown(answers);
+            writeToFile('./sample-readme', readmeContent);
+        });
 }
 
 promptUser();
